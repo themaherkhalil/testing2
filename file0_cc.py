2072f27c913764bcb5c8e0e503c1c616d1db9eac
mahkhalil <mahkhalil@deloitte.com>
2022-02-25 10:26:57 -0500

    adding code

---
+++ file0_cc.py
@@ -0,0 +1,6 @@
+###### Code executed from SplitUnpivotReactor #######
+Diabetes_FRAME621822w.split_unpivot('DRUG', ',_')
+Diabetes_FRAME621822 = Diabetes_FRAME621822w.cache['data']
+###### Code executed from SplitUnpivotReactor #######
+Diabetes_FRAME621822w.split_unpivot('DRUG', ',')
+Diabetes_FRAME621822 = Diabetes_FRAME621822w.cache['data']
