3a8c611b8a07cc1d34285744daf076bdfa6d1378
themaherkhalil <khalil.maher91@gmail.com>
2022-02-25 10:59:35 -0500

    Delete file0_cc.py

--- file0_cc.py
+++
@@ -1,6 +0,0 @@
-###### Code executed from SplitUnpivotReactor #######
-Diabetes_FRAME621822w.split_unpivot('DRUG', ',_')
-Diabetes_FRAME621822 = Diabetes_FRAME621822w.cache['data']
-###### Code executed from SplitUnpivotReactor #######
-Diabetes_FRAME621822w.split_unpivot('DRUG', ',')
-Diabetes_FRAME621822 = Diabetes_FRAME621822w.cache['data']
